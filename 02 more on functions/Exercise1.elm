39bc92713cbe812c8630ed6a217d432e75f643b7
Diogo Korok <korokd@protonmail.ch>
2020-08-25 22:05:14 -0300

    Indetify each Exercise result better

--- 02 more on functions/Exercise1.elm
+++ 02 more on functions/Exercise1.elm
@@ -1,11 +1,22 @@
 module Main exposing (main)
 
-import Html exposing (div, text)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
 
 
 (~=) : String -> String -> Bool
 (~=) leftString rightString =
     String.left 1 leftString == String.left 1 rightString
+
+
+makeExerciseText : Int -> Bool -> Html msg
+makeExerciseText exercise almostEqual =
+    div []
+        [ text ("Exercise " ++ Basics.toString exercise ++ ":")
+        , div []
+            [ text (Basics.toString almostEqual)
+            ]
+        ]
 
 
 main =
@@ -17,10 +28,10 @@ main =
             (~=) "abacate" "carambola"
     in
     div []
-        [ div []
-            [ text (Basics.toString calledInfix)
+        [ div [ style [("margin-bottom", "10px")] ]
+            [ makeExerciseText 1 calledInfix
             ]
         , div []
-            [ text (Basics.toString calledPrefix)
+            [ makeExerciseText 2 calledPrefix
             ]
         ]
